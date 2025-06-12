6111a045d6a61ced85e4271704f1ea885603d795
cursosisraelsp <cursos.israelsp@gmail.com>
2025-06-12 20:30:57 +0200

    Agregando tipografía correctamente

--- app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -22,24 +22,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 import com.example.woof.R
-/*
+
 val AbrilFatface = FontFamily(
-    Font(R.font.abril_fatface_regular)
+    Font(R.font.abrilfatface_regular)
 )
-*/
+
 val Montserrat = FontFamily(
     Font(R.font.montserrat_regular),
-    Font(R.font.montserrat_bold, FontWeight.Bold)
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+
 )
 // Set of Material typography styles to start with
 val Typography = Typography(
-    /*displayLarge = TextStyle(
-        fontFamily = AbrilFatface,
-        fontWeight = FontWeight.Normal,
-        fontSize = 36.sp
-    ),*/
     displayLarge = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = AbrilFatface,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp
     ),
